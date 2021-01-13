7580bc9a9af587ed01fbf62c380a9b7729bf045f
Rupert Bright <rupert.bright@live.co.uk>
2021-01-13 11:28:38 +0000

    fix: animation re-work

--- src/components/header/nav-links/nav_links.js
+++ src/components/header/nav-links/nav_links.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import Fade from 'react-reveal/Reveal';
+// import Fade from 'react-reveal/Reveal';
 import Cv from './rbcv.pdf'
 
 
@@ -8,14 +8,14 @@ function Nav_links() {
 
     return (
     <div>
-  <Fade cascade top>
+  {/* <Fade cascade top> */}
         <div className='navlinks-container'>
             <div><AnchorLink href='#about'><span>01.</span> About</AnchorLink></div>
             <div><AnchorLink href='#projects'><span>02.</span> Projects</AnchorLink></div>
             <div><AnchorLink href='#contact'><span>03.</span> Contact</AnchorLink></div>
             <button onClick={() => window.open(Cv)}>CV</button>
     </div>
-    </Fade>
+    {/* </Fade> */}
     </div>
         )
 
