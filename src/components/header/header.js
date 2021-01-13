7df5f5a6f0b1aac0c2dd2a3f0e7f90b1695a1b92
Rupert Bright <rupert.bright@live.co.uk>
2021-01-13 11:15:07 +0000

    feat: header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,15 +2,35 @@ import PropTypes from "prop-types"
 import React from "react"
 import NavLinks from './nav-links/nav_links'
 import Headroom from 'react-headroom'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { slide as Menu } from 'react-burger-menu'
+import Cv from '../header/nav-links/rbcv.pdf'
 
 
-const Header = () => (
-<Headroom disableInlineStyles={true}>
-  <header className='header-container'>
+function Header(window) {
+
+    // const window = Object.values(windowWidth)
+    // console.log('landing', window)
+
+  
+    console.log(window.window[0])
+
+return (
+    <div>
+{window.window[0]> 1000 || window === 'undefined'  ? (<Headroom><header className='header-container'>
   <NavLinks/>
   </header>
-  </Headroom>
+  </Headroom>) : ( 
+        <Menu  right>
+        <div ><AnchorLink href='#about'>About</AnchorLink></div>
+            <div ><AnchorLink href='#projects'>Projects</AnchorLink></div>
+            <div ><AnchorLink href='#contact'>Contact</AnchorLink></div>
+            <button onClick={() => window.open(Cv)} >CV</button>
+            </Menu>
+        )}
+        </div>
 )
+  }
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/sections/landing.js
+++ src/components/sections/landing.js
@@ -3,8 +3,7 @@ import Fade from 'react-reveal/Reveal';
 import {Arrow} from '../../assets/icons';
 import Header from '../header/header';
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { slide as Menu } from 'react-burger-menu'
-import Cv from '../header/nav-links/rbcv.pdf'
+
 
 function Landing(windowWidth) {
     const window = Object.values(windowWidth)
@@ -14,21 +13,13 @@ function Landing(windowWidth) {
 
     return (
         <div className='container'>
-        {window[0] > 1000 || window === 'undefined'  ? (<Header windowWidth ={windowWidth}/>) : ( 
-        <Menu  right>
-        <div ><AnchorLink href='#about'>About</AnchorLink></div>
-            <div ><AnchorLink href='#projects'>Projects</AnchorLink></div>
-            <div ><AnchorLink href='#contact'>Contact</AnchorLink></div>
-            <button onClick={() => window.open(Cv)} >CV</button>
-            </Menu>
-        )}
-        <Fade delay={4000}>
+        <Header window={window}/>
         <div className='rbdescription'>
         <h1><span>Rupert Bright.</span></h1>
-        <h1>From London based in Switzerland.</h1>
+        <h1>From London, based in Switzerland.</h1>
         <h1><span> Software Developer.</span></h1>
         </div>
-        </Fade>
+
         <div className='arrow-container'><AnchorLink href='#about'>
         <Arrow className='arrow'/>
         </AnchorLink>
