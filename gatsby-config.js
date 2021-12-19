afb76f9cd7c18a358cc29c7c79998bfa9229b955
Rupert Bright <rupert.bright@live.co.uk>
2021-12-19 13:24:39 +0100

    fonts added back in

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
       },
     },    
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
           {
